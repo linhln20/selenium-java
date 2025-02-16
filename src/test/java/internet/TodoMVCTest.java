e727bdb7738512da4646cf11e4b472a34bba881c
Linh Lam <ngoclinhlam95@gmail.com>
2025-02-16 22:13:10 +0700

    modified setup TodoMVC

--- src/test/java/internet/TodoMVCTest.java
+++ src/test/java/internet/TodoMVCTest.java
@@ -12,7 +12,7 @@ public class TodoMVCTest {
 
     @BeforeMethod
     void setUp(){
-        Browser.openBrowser("edge");
+        Browser.openBrowser("chrome");
         todoMVCPage = new TodoMVCPage();
         todoMVCPage.open();
 //        Browser.maximize();
