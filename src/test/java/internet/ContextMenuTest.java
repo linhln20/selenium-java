e50934250c418ff209b5f565a22a92acef1822c6
Linh Lam <ngoclinhlam95@gmail.com>
2025-03-15 15:42:59 +0700

    Add ContextMenuTest

--- src/test/java/internet/ContextMenuTest.java
+++ src/test/java/internet/ContextMenuTest.java
@@ -1,4 +1,27 @@
 package internet;
 
+import internet.pages.ContextMenuPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static supports.Browser.openBrowser;
+
 public class ContextMenuTest {
+    ContextMenuPage contextMenuPage;
+
+    @Parameters({"browser"})
+    @BeforeMethod
+    void setUp(String browser) {
+        openBrowser(browser);
+        contextMenuPage = new ContextMenuPage();
+        contextMenuPage.open();
+    }
+
+    @Test
+    void clickRight(){
+        contextMenuPage.clickRight();
+        Assert.assertEquals(contextMenuPage.getAlertText(), "You selected a context menu");
+    }
 }
--- src/test/java/internet/pages/ContextMenuPage.java
+++ src/test/java/internet/pages/ContextMenuPage.java
@@ -1,4 +1,28 @@
 package internet.pages;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import supports.Browser;
+
+import static supports.Browser.getElement;
+import static supports.Browser.visit;
+
 public class ContextMenuPage {
+
+    public void open(){
+        visit("https://the-internet.herokuapp.com/context_menu");
+    }
+
+    public void clickRight(){
+        WebElement target = getElement(By.id("hot-spot"));
+        Actions actions = new Actions(Browser.getDriver());
+        actions.contextClick(target).perform();
+    }
+
+    public String getAlertText(){
+        Alert alert = Browser.getDriver().switchTo().alert();
+        return alert.getText();
+    }
 }
