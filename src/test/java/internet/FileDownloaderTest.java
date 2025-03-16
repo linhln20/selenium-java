32a0c804c2ba25002e34a61eac32c66029a3beaa
Linh Lam <ngoclinhlam95@gmail.com>
2025-03-16 17:44:27 +0700

    Add FileDownloaderTest

---
+++ src/test/java/internet/FileDownloaderTest.java
@@ -0,0 +1,35 @@
+package internet;
+
+import internet.pages.FileDownloaderPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static supports.Browser.openBrowser;
+import static supports.Browser.quit;
+
+public class FileDownloaderTest {
+    FileDownloaderPage fileDownloaderPage;
+
+    @Parameters({"browser"})
+    @BeforeMethod
+    void setUp(String browser) {
+        openBrowser(browser);
+        fileDownloaderPage = new FileDownloaderPage();
+        fileDownloaderPage.open();
+    }
+
+    @Test
+    void downloaderFile() throws InterruptedException {
+        fileDownloaderPage.clickDownloadFile();
+        fileDownloaderPage.isFileDownload();
+    }
+    @AfterMethod
+    void tearDown() {
+        quit();
+    }
+}
---
+++ src/test/java/internet/pages/FileDownloaderPage.java
@@ -0,0 +1,38 @@
+package internet.pages;
+
+import supports.Browser;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static supports.Browser.visit;
+
+public class FileDownloaderPage {
+    String fileName = "selenide-intro.txt";
+    String downloadPath = System.getProperty("user.home") + "/Downloads";
+
+    public void open() {
+        visit("https://the-internet.herokuapp.com/download");
+    }
+
+    public void clickDownloadFile() {
+        Browser.clickText(fileName);
+    }
+
+    public void isFileDownload () throws InterruptedException {
+        File downloadedFile = Paths.get(downloadPath, fileName).toFile();
+        int waitTime = 10;
+        while (waitTime > 0) {
+            if (downloadedFile.exists()) {
+                System.out.println("Successfully download: " + downloadedFile.getAbsolutePath());
+                break;
+            }
+            Thread.sleep(1000);
+            waitTime--;
+        }
+
+        if (!downloadedFile.exists()) {
+            System.out.println("Download failed");
+        }
+    }
+}
