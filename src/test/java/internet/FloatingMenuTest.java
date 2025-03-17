674f5e269924d529d20805bc793f03793c426417
Linh Lam <ngoclinhlam95@gmail.com>
2025-03-17 20:55:33 +0700

    Add FloatingMenuTest

--- src/test/java/internet/FloatingMenuTest.java
+++ src/test/java/internet/FloatingMenuTest.java
@@ -1,4 +1,34 @@
 package internet;
 
+import internet.pages.FloatingMenuPage;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static supports.Browser.openBrowser;
+
 public class FloatingMenuTest {
+    FloatingMenuPage floatingMenuPage;
+
+    @Parameters({"browser"})
+    @BeforeMethod
+    void setUp(String browser) {
+        openBrowser(browser);
+        floatingMenuPage = new FloatingMenuPage();
+        floatingMenuPage.open();
+    }
+
+    @Test
+    void verifyFloatingMenuVisibility() {
+        WebElement floatMenu = floatingMenuPage.getFloatingMenu();
+        Assert.assertTrue(floatingMenuPage.getFloatingMenu().isDisplayed());
+    }
+
+    @Test
+    void verifyFloatingMenuStaysVisible() {
+        floatingMenuPage.scroll();
+        Assert.assertTrue(floatingMenuPage.getFloatingMenu().isDisplayed());
+    }
 }
--- src/test/java/internet/pages/FloatingMenuPage.java
+++ src/test/java/internet/pages/FloatingMenuPage.java
@@ -1,4 +1,24 @@
 package internet.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import static supports.Browser.*;
+
 public class FloatingMenuPage {
+    private JavascriptExecutor js;
+    public void open(){
+        visit("http://the-internet.herokuapp.com/floating_menu");
+    }
+
+    public WebElement getFloatingMenu(){
+        WebElement floatingMenu = getElement(By.id("menu"));
+        return floatingMenu;
+    }
+
+    public void scroll(){
+        js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
 }
